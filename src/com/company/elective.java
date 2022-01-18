233bb39dc7e2ac6aa6b490d53316183e55ea3aea
Lyapese <kamilla.bu4@gmail.com>
2022-01-18 12:07:45 +0300

    4

--- src/com/company/elective.java
+++ src/com/company/elective.java
@@ -17,6 +17,5 @@ public class elective {
     }
     public void getListParrents()
     {
-
     }
 }
--- src/com/company/school.java
+++ src/com/company/school.java
@@ -21,24 +21,27 @@ public class school {
         Sections = Se;
     }
 
-    public void getListTeachers()
-    {
-
+    public void getListTeachers() {System.out.println(Teachers);}
+    public void getListEmployeers() {System.out.println(Employees);}
+    public void getListLearners() {System.out.println(Learners);}
+    public void getElectronicJournal() {
     }
-    public void getListEmployeers()
-    {
-
-    }
-    public void getListLearners()
-    {
-
-    }
-    public void getElectronicJournal()
-    {
-
-    }
-    public Participant getParticipant()
-    {
-        return ;
+    public Participant getParticipant(int CardID) {
+        for (int i = 0; i<Employees.length; i++){
+            if (Employees[i].CardID == CardID){
+                return Employees[i];
+            }
+        }
+        for (int i = 0; i < Teachers.length; i++) {
+            if (Teachers[i].CardID == CardID) {
+                return Teachers[i];
+            }
+        }
+        for (int i = 0; i < Learners.length; i++){
+            if (Learners[i].CardID == CardID){
+                return Learners[i];
+            }
+        }
+        return null;
     }
 }
